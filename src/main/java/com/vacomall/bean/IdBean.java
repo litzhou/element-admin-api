391bdacf9b2b7cf77a7fc24b19b37e9d4298b368
litzhou <litzhou@qq.com>
2018-04-13 12:07:01 +0800

    update user

---
+++ src/main/java/com/vacomall/bean/IdBean.java
@@ -0,0 +1,21 @@
+package com.vacomall.bean;
+
+import java.io.Serializable;
+
+public class IdBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String[] ids;
+
+	public String[] getIds() {
+		return ids;
+	}
+
+	public void setIds(String[] ids) {
+		this.ids = ids;
+	}
+	
+}
--- src/main/java/com/vacomall/controller/UserController.java
+++ src/main/java/com/vacomall/controller/UserController.java
@@ -1,16 +1,19 @@
 package com.vacomall.controller;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
+import com.vacomall.bean.IdBean;
 import com.vacomall.bean.Rest;
 import com.vacomall.entity.User;
 import com.vacomall.service.UserService;
@@ -27,47 +30,57 @@ import com.vacomall.service.UserService;
 @RequestMapping("/user")
 public class UserController {
 
-	@Autowired private UserService userService;
-	
+	@Autowired
+	private UserService userService;
+
 	/**
 	 * 查询用户列表
+	 * 
 	 * @param page
 	 * @param size
 	 * @param search
 	 * @return
 	 */
-	@GetMapping("/list")
-	public Rest list(@RequestParam(value = "current", defaultValue = "1") int current,
+	@GetMapping("/page")
+	public Rest page(@RequestParam(value = "current", defaultValue = "1") int current,
 			@RequestParam(value = "size", defaultValue = "10") int size, String search) {
-		
+
 		EntityWrapper<User> wrapper = new EntityWrapper<User>();
-		if(StringUtils.isNotBlank(search)){
+		if (StringUtils.isNotBlank(search)) {
 			wrapper.like("userName", search);
 		}
 		Page<User> page = userService.selectPage(new Page<User>(current, size), wrapper);
 		return Rest.okData(page);
 	}
+
+	@GetMapping("/list")
+	public Rest list() {
+		return Rest.okData(userService.selectList(null));
+	}
+
 	/**
 	 * 删除用户
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@DeleteMapping("/delete")
-	public Rest list (String id) {
-		if(StringUtils.isBlank(id)){
+	public Rest delete(@RequestBody IdBean idBean) {
+		if (idBean == null || ArrayUtils.isEmpty(idBean.getIds())) {
 			return Rest.failure("客户端传入ID为空");
 		}
-		userService.deleteById(id);
-		return Rest.ok();
+		//userService.deleteById(ids);
+		return Rest.ok("删除成功");
 	}
-	
+
 	/**
 	 * 修改
+	 * 
 	 * @param user
 	 * @return
 	 */
 	@PutMapping("/update")
-	public Rest list (User user) {
+	public Rest list(User user) {
 		userService.updateById(user);
 		return Rest.ok();
 	}
